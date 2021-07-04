1b4e9b675a6752d19a9b2b6e890fa99381bf434b
Paulo Cesar <paulocitron99@gmail.com>
2021-07-04 19:13:20 -0300

    reset main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css'
 
-import Table from './components/Table'
-
 function App() {
-
-  const showAlert = (str) => {
-    alert(str)
-  }
 
   return (
     <div className="App">
-      <Table cabecalho={['Nome','Idade','Actions']} dados={[{nome: 'Jorge', idade: '32'},{nome: 'Paulo', idade: '21'}]} showAlert={showAlert} />
+      <p>oi</p>
     </div>
   )
 }
